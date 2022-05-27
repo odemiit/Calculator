92b0cf87e4e08f7df02f5195052c330bc99077f8
Odemi Tosanwumi <odemi24@my.yorku.ca>
2022-05-27 17:21:46 -0400

    Sending HTML file sending the html file to the server to tell it what to display

--- calculator.js
+++ calculator.js
@@ -5,7 +5,8 @@ const app = express();
 
 
 app.get("/", function(req, res) {
-    res.send("Hello World");
+    //res.send("Hello World");
+    res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(3000, function() {
