a4de7f6cf1d16c9ed72113538f5a6efadff3b1ed
Odemi Tosanwumi <odemi24@my.yorku.ca>
2022-05-27 18:14:25 -0400

    Get and Post methods Added get and post methods for the bmiCalculator to the server

--- calculator.js
+++ calculator.js
@@ -22,6 +22,16 @@ app.post("/", function(req, res) {
     res.send("The result of the calculation is " + result);
 });
 
+app.get("/bmicalculator" , function(req, res) {
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function (req, res) {
+
+    var weight = Number(req.body.weight);
+    var height = Number(req.body.height);
+})
+
 app.listen(3000, function() {
     console.log("Server started on port 3000");
 });
