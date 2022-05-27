e6c6c65e2704d674b7ea9bfef14ea5a4424a4c34
Odemi Tosanwumi <odemi24@my.yorku.ca>
2022-05-27 17:49:18 -0400

    Post Method Added the "post" to the JS file so the server knows what to do when - something is posted to the home page.

--- calculator.js
+++ calculator.js
@@ -9,6 +9,10 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
+app.post("/", function(req, res) {
+    res.send("Thanks for posting that!");
+});
+
 app.listen(3000, function() {
     console.log("Server started on port 3000");
 });
