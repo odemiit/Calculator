87b3cda9be03f80ee61518579012d5115bcd2254
Odemi Tosanwumi <46735063+odemiit@users.noreply.github.com>
2024-07-10 17:17:25 +0000

    defining the basic functions of the calculator

---
+++ main.py
@@ -0,0 +1,17 @@
+#Calculator
+
+#Add Function: Takes 2 numbers and adds them together
+def add(n1, n2):
+  return n1 + n2
+  
+#Subtract Function: Takes 2 numbers and subtracts one from the other
+def subtract(n1, n2):
+  return n1 - n2
+  
+#Multiply Function: Takes 2 numbers and multiplies them together
+def multiply(n1, n2):
+  return n1 * n2
+  
+#Divide Function: Takes 2 numbers and divides one by the other
+def divide(n1, n2):
+  return n1 / n2
